8b1e4ebfb3888dc6efe90e9a26956874166f0a4e
kristensala <salakristen1@gmail.com>
2025-05-05 23:09:58 +0300

    fix lsp config; stop using lsp-zero and replace cmp with blink.cmp

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,23 +1,9 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
+    'neovim/nvim-lspconfig',
     dependencies = {
         -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
         {'williamboman/mason.nvim'},           -- Optional
         {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},         -- Required
-        {'hrsh7th/cmp-nvim-lsp'},     -- Required
-        {'hrsh7th/cmp-buffer'},       -- Optional
-        {'hrsh7th/cmp-path'},         -- Optional
-        {'saadparwaiz1/cmp_luasnip'}, -- Optional
-        {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},             -- Required
-        {'rafamadriz/friendly-snippets'}, -- Optional
     },
     config = function()
         vim.opt.signcolumn = 'yes'
@@ -49,7 +35,9 @@ return {
             ensure_installed = {
                 "html",
                 "ols",
-                "ts_ls"
+                "ts_ls",
+                "gopls",
+                "cssls"
             },
             handlers = {
                 function(server_name)
@@ -57,47 +45,5 @@ return {
                 end,
             }
         })
-
-        local cmp = require('cmp')
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
-        cmp.setup({
-            sources = {
-                {name = "path"},
-                {name = "nvim_lsp"},
-                {name = "nvim_lua"},
-            },
-            --formatting = lsp.cmp_format(),
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-        })
-
-        local lsp = require("lsp-zero")
-        lsp.configure('eslint', {
-            filetypes = {
-                "javascript",
-                "javascriptreact",
-                "javascript.jsx",
-                "typescript",
-                "typescriptreact",
-                "typescript.tsx",
-            }
-        })
-
-        lsp.configure('ts_ls', {
-            filetypes = {
-                "javascript",
-                "javascriptreact",
-                "javascript.jsx",
-                "typescript",
-                "typescriptreact",
-                "typescript.tsx",
-            }
-        })
-
     end
 }
