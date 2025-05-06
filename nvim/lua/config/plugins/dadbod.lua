ca02a5418d1603370528ab65f0c8a577abc2b844
kristensala <salakristen1@gmail.com>
2025-05-06 10:13:24 +0300

    add dadbod local db connections for work

--- nvim/lua/config/plugins/dadbod.lua
+++ nvim/lua/config/plugins/dadbod.lua
@@ -12,5 +12,12 @@ return {
   },
   init = function()
     -- Your DBUI configuration
+    vim.g.dbs = {
+        { name = 'sais@users', url = 'postgres://sais:sais@localhost:5432/Users' },
+        { name = 'sais@admission', url = 'postgres://sais:sais@localhost:5437/Admissions' },
+        { name = 'sais@exam', url = 'postgres://sais:sais@localhost:5438/Exams' },
+        { name = 'sais@settings', url = 'postgres://sais:sais@localhost:5433/Settings' },
+        { name = 'sais@messages', url = 'postgres://sais:sais@localhost:5434/Messages' },
+    }
   end,
 }
